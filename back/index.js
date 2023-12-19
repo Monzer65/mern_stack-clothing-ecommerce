e68ac94ed487d81d3666e77d9b0a955eb7730d30
Monzer65 <105078122+Monzer65@users.noreply.github.com>
2023-12-19 23:52:56 +0330

    Update index.js

--- back/index.js
+++ back/index.js
@@ -22,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
 app.get("/", (req, res) => {
   res.send("Server is running");
