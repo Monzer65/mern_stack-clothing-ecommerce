521aac794f466f8465a14169ce414ca4f0bd5cb8
Monzer65 <105078122+Monzer65@users.noreply.github.com>
2023-12-19 22:41:27 +0330

    Update apiSlice.js

--- front/src/reducers/apiSlice.js
+++ front/src/reducers/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logout } from "../reducers/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://mern-shop-api-tau.vercel.app",
+  baseUrl: "https://mern-shop-ry2l.vercel.app",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
