1114ed36f5925864596f0a257827835ac5a79a73
monzer <danesh.monzer@gmail.com>
2023-12-19 19:15:53 +0330

    cors update again

--- back/config/allowedOrigins.js
+++ back/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  "https://mern-shop-api-tau.vercel.app",
+  "https://mern-shop-seven-pi.vercel.app",
+  "https://mern-shop-git-main-monzers-projects.vercel.app/",
   "http://127.0.0.1:5500",
 ];
 
