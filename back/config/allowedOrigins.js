dc9c1256e4563c09bbc93770ccbed5e549662dbb
Monzer65 <105078122+Monzer65@users.noreply.github.com>
2023-12-19 19:28:39 +0330

    Update allowedOrigins.js

--- back/config/allowedOrigins.js
+++ back/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   "https://mern-shop-api-tau.vercel.app",
-  "http://127.0.0.1:5500",
+  https://mern-shop-git-main-monzers-projects.vercel.app
 ];
 
 module.exports = allowedOrigins;
