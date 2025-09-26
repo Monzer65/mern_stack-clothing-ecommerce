55d85810eb165639649d0414c3ee480aa9745a91
Monzer65 <105078122+Monzer65@users.noreply.github.com>
2025-09-26 09:48:22 +0330

    Update dbUri.js
    uri updated

--- back/config/dbUri.js
+++ back/config/dbUri.js
@@ -1,2 +1,3 @@
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.50cmhcw.mongodb.net/shop?retryWrites=true&w=majority`;
+{/*const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.50cmhcw.mongodb.net/shop?retryWrites=true&w=majority`;*/}
+const uri = `mongodb+srv://monzer:${process.env.MONGODB_PASS}@cluster0.50cmhcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 module.exports = uri;
